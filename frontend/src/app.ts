921bd9fb4b2d58a902ecb890e3b7154208d52fd8
Daven Quinn <dev@davenquinn.com>
2022-05-27 00:38:20 -0500

    Updated storage system

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,9 +1,19 @@
 import { Group, Text, useMantineTheme, MantineTheme } from "@mantine/core";
 import { Dropzone, DropzoneStatus, MIME_TYPES } from "@mantine/dropzone";
 import h from "@macrostrat/hyper";
+import { SupabaseStorageClient } from "@supabase/storage-js";
+
+const STORAGE_URL = "http://locahost:8000/api/storage";
+const SERVICE_KEY = "service"; //! service key, not anon key
+
+const storageClient = new SupabaseStorageClient(STORAGE_URL, {
+  apikey: SERVICE_KEY,
+  Authorization: `Bearer ${SERVICE_KEY}`,
+});
 
 function onDrop(files: File[]) {
-  console.log(files);
+  const file = files[0];
+  console.log(file);
 }
 
 function ImageUploader() {
@@ -12,6 +22,7 @@ function ImageUploader() {
     {
       accept: [MIME_TYPES.png, MIME_TYPES.jpeg, MIME_TYPES.svg, MIME_TYPES.gif],
       onDrop,
+      multiple: false,
     },
     (status: DropzoneStatus) => {
       return h("p", null, "Drop an image here");
