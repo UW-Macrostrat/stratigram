de29b8b763bd03ec026d2444e46445521f8fb935
Daven Quinn <dev@davenquinn.com>
2022-05-28 04:06:33 -0500

    Nicer formatting for storage sql files

--- schema/03-storage-system.sql
+++ schema/03-storage-system.sql
@@ -5,6 +5,7 @@
  */
 CREATE SCHEMA IF NOT EXISTS storage;
 
+
 CREATE TABLE storage.migrations (
   id integer NOT NULL,
   name character varying(100) NOT NULL,
@@ -12,103 +13,105 @@ CREATE TABLE storage.migrations (
   executed_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS "storage"."buckets" (
-  "id" text not NULL,
-  "name" text NOT NULL,
-  "owner" uuid,
-  "created_at" timestamptz DEFAULT now(),
-  "updated_at" timestamptz DEFAULT now(),
-  CONSTRAINT "buckets_owner_fkey" FOREIGN KEY ("owner") REFERENCES "auth"."users"("id"),
-  PRIMARY KEY ("id")
+
+CREATE TABLE IF NOT EXISTS storage.buckets (
+  id text NOT NULL PRIMARY KEY,
+  name text NOT NULL,
+  owner uuid REFERENCES auth.users(id),
+  created_at timestamptz DEFAULT NOW(),
+  updated_at timestamptz DEFAULT NOW()
 );
 
-CREATE UNIQUE INDEX "bname" ON "storage"."buckets" USING BTREE ("name");
+CREATE UNIQUE INDEX bname ON storage.buckets USING BTREE (name);
 
-CREATE TABLE IF NOT EXISTS "storage"."objects" (
-  "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-  "bucket_id" text,
-  "name" text,
-  "owner" uuid,
-  "created_at" timestamptz DEFAULT now(),
-  "updated_at" timestamptz DEFAULT now(),
-  "last_accessed_at" timestamptz DEFAULT now(),
-  "metadata" jsonb,
-  CONSTRAINT "objects_bucketId_fkey" FOREIGN KEY ("bucket_id") REFERENCES "storage"."buckets"("id"),
-  CONSTRAINT "objects_owner_fkey" FOREIGN KEY ("owner") REFERENCES "auth"."users"("id"),
-  PRIMARY KEY ("id")
+
+CREATE TABLE IF NOT EXISTS storage.objects (
+  id uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+  bucket_id text REFERENCES storage.buckets(id),
+  name text,
+  owner uuid REFERENCES auth.users(id),
+  created_at timestamptz DEFAULT NOW(),
+  updated_at timestamptz DEFAULT NOW(),
+  last_accessed_at timestamptz DEFAULT NOW(),
+  metadata jsonb
 );
 
-CREATE UNIQUE INDEX "bucketid_objname" ON "storage"."objects" USING BTREE ("bucket_id", "name");
+CREATE UNIQUE INDEX bucketid_objname ON storage.objects USING BTREE (bucket_id, name);
 
 CREATE INDEX name_prefix_search ON storage.objects(name text_pattern_ops);
 
-
 ALTER TABLE storage.objects ENABLE ROW LEVEL SECURITY;
 
+
 CREATE OR REPLACE FUNCTION storage.foldername(name text)
- RETURNS text[]
- LANGUAGE plpgsql
-AS $function$
-DECLARE
-_parts text[];
+RETURNS text [] LANGUAGE plpgsql AS $$
+DECLARE _parts text [];
 BEGIN
-	select string_to_array(name, '/') into _parts;
-	return _parts[1:array_length(_parts,1)-1];
-END
-$function$;
+SELECT string_to_array(name, '/') INTO _parts;
+RETURN _parts [1:array_length(_parts,1)-1];
+END $$;
+
 
 CREATE OR REPLACE FUNCTION storage.filename(name text)
- RETURNS text
- LANGUAGE plpgsql
-AS $function$
-DECLARE
-_parts text[];
+RETURNS text
+LANGUAGE plpgsql
+AS $$
+DECLARE _parts text [];
 BEGIN
-	select string_to_array(name, '/') into _parts;
-	return _parts[array_length(_parts,1)];
-END
-$function$;
+SELECT string_to_array(name, '/') INTO _parts;
+RETURN _parts [array_length(_parts,1)];
+END $$;
+
 
 CREATE OR REPLACE FUNCTION storage.extension(name text)
- RETURNS text
- LANGUAGE plpgsql
-AS $function$
+RETURNS text LANGUAGE plpgsql AS $$
 DECLARE
-_parts text[];
-_filename text;
+  _parts text [];
+  _filename text;
 BEGIN
-	select string_to_array(name, '/') into _parts;
-	select _parts[array_length(_parts,1)] into _filename;
-	-- @todo return the last part instead of 2
-	return reverse(split_part(reverse(_filename), '.', 1));
-END
-$function$;
+SELECT string_to_array(name, '/') INTO _parts;
+SELECT _parts [array_length(_parts,1)] INTO _filename;
+-- @todo return the last part instead of 2
+RETURN reverse(split_part(reverse(_filename), '.', 1));
+END $$;
+
 
 -- @todo can this query be optimised further?
-CREATE OR REPLACE FUNCTION storage.search(prefix text, bucketname text, limits int DEFAULT 100, levels int DEFAULT 1, offsets int DEFAULT 0)
- RETURNS TABLE (
-    name text,
-    id uuid,
-    updated_at TIMESTAMPTZ,
-    created_at TIMESTAMPTZ,
-    last_accessed_at TIMESTAMPTZ,
-    metadata jsonb
-  )
- LANGUAGE plpgsql
-AS $function$
-BEGIN
-	return query 
-		with files_folders as (
-			select ((string_to_array(objects.name, '/'))[levels]) as folder
-			from objects
-			where objects.name ilike prefix || '%'
-			and bucket_id = bucketname
-			GROUP by folder
-			limit limits
-			offset offsets
-		) 
-		select files_folders.folder as name, objects.id, objects.updated_at, objects.created_at, objects.last_accessed_at, objects.metadata from files_folders 
-		left join objects
-		on prefix || files_folders.folder = objects.name and objects.bucket_id=bucketname;
-END
-$function$;
+CREATE OR REPLACE FUNCTION storage.search(
+  prefix text,
+  bucketname text,
+  limits int DEFAULT 100,
+  levels int DEFAULT 1,
+  offsets int DEFAULT 0
+) RETURNS TABLE (
+  name text,
+  id uuid,
+  updated_at TIMESTAMPTZ,
+  created_at TIMESTAMPTZ,
+  last_accessed_at TIMESTAMPTZ,
+  metadata jsonb
+) LANGUAGE plpgsql AS $$
+BEGIN RETURN query
+WITH files_folders AS (
+  SELECT
+    ((string_to_array(objects.name, '/')) [levels]) AS folder
+  FROM objects
+  WHERE
+    objects.name ilike prefix || '%'
+    AND bucket_id = bucketname
+  GROUP BY
+    folder
+  LIMIT
+    limits OFFSET offsets
+)
+SELECT
+  files_folders.folder AS name,
+  objects.id,
+  objects.updated_at,
+  objects.created_at,
+  objects.last_accessed_at,
+  objects.metadata
+FROM files_folders
+  LEFT JOIN objects ON prefix || files_folders.folder = objects.name
+  AND objects.bucket_id = bucketname;
+END $$;
--- schema/06-api-schema.sql
+++ schema/06-api-schema.sql
@@ -1,15 +1,3 @@
 DROP SCHEMA stratiform_api CASCADE;
 
 CREATE SCHEMA stratiform_api;
-
-CREATE VIEW stratiform_api.objects AS
-SELECT
-  *
-FROM
-  storage.objects;
-
-CREATE VIEW stratiform_api.buckets AS
-SELECT
-  *
-FROM
-  storage.buckets;
