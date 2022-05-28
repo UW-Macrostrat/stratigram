d237d2b1c0e91f081556ea0f1509b5d29f4d004f
Daven Quinn <dev@davenquinn.com>
2022-05-28 02:13:56 -0500

    Updated headers etc.

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,19 +1,23 @@
-import { Group, Text, useMantineTheme, MantineTheme } from "@mantine/core";
+import { Group } from "@mantine/core";
 import { Dropzone, DropzoneStatus, MIME_TYPES } from "@mantine/dropzone";
+import { useAsyncEffect, DeleteButton } from "@macrostrat/ui-components";
 import h from "@macrostrat/hyper";
-import { StorageClient } from "@supabase/storage-js";
-import { useState, useEffect } from "react";
+import { StorageClient, FileObject } from "@supabase/storage-js";
+import { useState } from "react";
 
-const storageClient = new StorageClient(process.env.STORAGE_URL, {
+const storageURL = process.env.STORAGE_URL;
+const storageToken = process.env.STORAGE_TOKEN;
+
+const storageClient = new StorageClient(storageURL, {
   //apikey: SERVICE_KEY,
   // JSON web token key
-  Authorization: `Bearer ${process.env.STORAGE_TOKEN}`,
+  Authorization: `Bearer ${storageToken}`,
 });
+
+const client = storageClient.from("section_images");
 
 function onDrop(files: File[]) {
   const file = files[0];
-  const client = storageClient.from("section_images");
-  console.log(file);
   client
     .upload(file.name, file, {
       upsert: true,
@@ -23,11 +27,18 @@ function onDrop(files: File[]) {
     });
 }
 
+const acceptedMimeTypes = [
+  MIME_TYPES.png,
+  MIME_TYPES.jpeg,
+  MIME_TYPES.svg,
+  MIME_TYPES.gif,
+];
+
 function ImageUploader() {
   return h(
     Dropzone,
     {
-      accept: [MIME_TYPES.png, MIME_TYPES.jpeg, MIME_TYPES.svg, MIME_TYPES.gif],
+      accept: acceptedMimeTypes,
       onDrop,
       multiple: false,
     },
@@ -37,22 +48,66 @@ function ImageUploader() {
   );
 }
 
-function useFileList() {
-  const [files, setFiles] = useState<File[]>();
-  useEffect(() => {
-    storageClient
-      .from("section_images")
-      .list()
-      .then((res) => {
-        console.log(res);
-        setFiles(res);
-      });
+type ImageData = FileObject & { publicURL: string };
+
+function useFileList(bucketName: string) {
+  const [files, setFiles] = useState<ImageData[]>([]);
+  useAsyncEffect(async () => {
+    let client = storageClient.from(bucketName);
+    let res = await client.list();
+    if (res.data == null) {
+      return;
+    }
+
+    let fileData: ImageData[] = [];
+
+    for (const file of res.data) {
+      const { publicURL } = client.getPublicUrl(file.name);
+      fileData.push({ ...file, publicURL });
+    }
+
+    setFiles(fileData);
   }, []);
   return files;
 }
 
+function Image({ image }) {
+  if (!acceptedMimeTypes.includes(image.metadata?.mimetype)) {
+    return h("p", null, "Not an image");
+  }
+  return h("img", { width: 200, src: image.publicURL });
+}
+
+function ImageFileManager({ image }) {
+  return h("div", [
+    h(Image, { image }),
+    h(DeleteButton, {
+      itemDescription: "this photo",
+      handleDelete: () => {
+        client.remove([image.name]);
+      },
+    }),
+  ]);
+}
+
+function ImageList({ images }: { images: ImageData[] }) {
+  if (images == null) return null;
+
+  return h(
+    Group,
+    {
+      direction: "row",
+      justify: "center",
+      align: "center",
+    },
+    images.map((image) => {
+      return h(ImageFileManager, { image });
+    })
+  );
+}
+
 function ImageManager() {
-  const img = useFileList();
+  const images = useFileList("section_images");
   return h(
     Group,
     {
@@ -61,7 +116,7 @@ function ImageManager() {
       justify: "center",
       align: "center",
     },
-    [h("img", { src: "https://picsum.photos/200" }), h(ImageUploader)]
+    [h(ImageList, { images }), h(ImageUploader)]
   );
 }
 
