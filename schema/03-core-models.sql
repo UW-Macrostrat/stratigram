494388e0141d6382271a88a23e331e8397beb5e6
Daven Quinn <dev@davenquinn.com>
2022-05-29 04:43:01 -0500

    Built a set of core models

--- schema/03-core-models.sql
+++ schema/03-core-models.sql
@@ -6,9 +6,78 @@ CREATE TABLE IF NOT EXISTS stratiform.project (
   description text
 );
 
-CREATE TABLE IF NOT EXISTS stratiform.section (
+-- Facies
+CREATE TABLE IF NOT EXISTS stratiform.facies_model (
   id serial PRIMARY KEY,
+  project_id integer NOT NULL REFERENCES stratiform.project(id),
+  name text NOT NULL,
+  description text
+);
+
+CREATE TABLE IF NOT EXISTS stratiform.facies (
+  id serial PRIMARY KEY,
+  model_id integer NOT NULL REFERENCES stratiform.facies_model(id),
   name text NOT NULL,
   description text,
-  project_id integer NOT NULL REFERENCES stratiform.project(id)
+  color text
 );
+
+CREATE TABLE IF NOT EXISTS stratiform.lithology (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  project_id integer REFERENCES stratiform.project(id),
+  description text,
+  pattern text
+);
+
+CREATE TABLE IF NOT EXISTS stratiform.grainsize (
+  key text PRIMARY KEY,
+  name text NOT NULL,
+  description text
+);
+
+-- Columns
+CREATE TABLE IF NOT EXISTS stratiform.column (
+  id serial PRIMARY KEY,
+  project_id integer NOT NULL REFERENCES stratiform.project(id),
+  name text NOT NULL,
+  description text,
+  geometry geometry(Geometry, 4326)
+);
+
+CREATE TABLE IF NOT EXISTS stratiform.column_surface (
+  id serial PRIMARY KEY,
+  height numeric NOT NULL,
+  column_id integer NOT NULL REFERENCES stratiform.column(id) ON DELETE CASCADE,
+  lithology_id integer REFERENCES stratiform.lithology(id),
+  facies_id integer REFERENCES stratiform.facies(id),
+  grainsize text REFERENCES stratiform.grainsize(key),
+  covered boolean,
+  schematic boolean,
+  surface_type text,
+  CHECK (surface_type IN ('sharp', 'gradational'))
+);
+
+CREATE TABLE IF NOT EXISTS stratiform.column_observation (
+  id serial PRIMARY KEY,
+  height numeric NOT NULL,
+  column_id integer NOT NULL REFERENCES stratiform.column(id) ON DELETE CASCADE,
+  top_height numeric,
+  note text,
+  symbol text
+);
+
+INSERT INTO stratiform.grainsize (key, name)
+VALUES
+  ('ms', 'Mudstone'),
+  ('ss', 'Siltstone'),
+  ('vf', 'Very fine sand'),
+  ('f', 'Fine sand'),
+  ('m', 'Medium sand'),
+  ('c', 'Coarse sand'),
+  ('p', 'Pebble'),
+  ('c', 'Cobble'),
+  ('b', 'Boulder'),
+  ('ws', 'Wackestone'),
+  ('ps', 'Packstone'),
+  ('gs', 'Grainstone');
---
+++ test-data/Zebra-Nappe-Section-J/insert-data.sql
@@ -0,0 +1,10 @@
+DO $$
+DECLARE
+  project_id integer;
+  section_id integer;
+  facies_model_id integer;
+BEGIN
+
+
+END
+$$;
