38b1fb07f2254144270805240b0f157e358ebbfc
Daven Quinn <dev@davenquinn.com>
2022-05-29 05:47:43 -0500

    Successfully import surfaces and notes

--- test-data/Zebra-Nappe-Section-J/insert-data.sql
+++ test-data/Zebra-Nappe-Section-J/insert-data.sql
@@ -28,11 +28,46 @@ FROM stratiform_import.facies
 ON CONFLICT (model_id, name)
 DO UPDATE SET description = EXCLUDED.description, color = EXCLUDED.color;
 
-INSERT INTO stratiform.lithology (name, pattern)
-SELECT id, pattern
+INSERT INTO stratiform.lithology (project_id, name, pattern)
+SELECT _project_id, id, pattern
 FROM stratiform_import.lithology
 ON CONFLICT (project_id, name) DO UPDATE
 SET pattern = EXCLUDED.pattern;
 
+DELETE FROM stratiform.column_surface WHERE column_id = _column_id;
+
+INSERT INTO stratiform.column_surface (
+  column_id, height, lithology_id, facies_id, grainsize, covered, surface_type)
+SELECT
+  _column_id,
+	s.height,
+	l.id lithology_id,
+	f.id facies_id,
+	grainsize,
+  CASE WHEN covered
+    THEN true
+  ELSE NULL
+  END covered,
+  CASE WHEN definite_boundary
+    THEN 'sharp'
+  WHEN (NOT definite_boundary)
+    THEN 'gradational'
+  ELSE NULL
+  END surface_type
+FROM stratiform_import.surface s
+JOIN stratiform.lithology l
+  ON l.name = s.lithology
+JOIN stratiform.facies f
+  ON f.name = s.facies;
+
+DELETE FROM stratiform.column_obs WHERE column_id = _column_id;
+
+INSERT INTO stratiform.column_obs (
+  column_id, height, top_height, note, symbol)
+SELECT _column_id, height, top_height, note, symbol
+FROM stratiform_import.note;
+
 END
 $$;
+
+DROP SCHEMA IF EXISTS stratiform_import CASCADE;
